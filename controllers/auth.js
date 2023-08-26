169198dd51c40b0ba97f58f779ec0ecfc1227052
Bogomil Simitchiev <simitchiev365@abv.bg>
2023-08-26 17:45:53 +0300

    Added changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -211,7 +211,7 @@ exports.postReset = (req, res, next) => {
           subject: 'Password reset',
           html: `
             <p>You requested a password reset</p>
-            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+            <p>Click this <a href="https://shoprush.onrender.com/reset/${token}">link</a> to set a new password.</p>
           `
         });
       })
